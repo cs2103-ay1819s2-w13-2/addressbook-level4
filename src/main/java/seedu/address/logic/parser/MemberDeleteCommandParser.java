ce38f3134b622910940b27f0d96c47b878b8dbc2
joel0111 <joel21_94@hotmail.com>
2019-02-27 17:59:12 +0800

    Updated DeleteCommand to MemberDeleteCommand and other variable name

--- src/main/java/seedu/address/logic/parser/MemberDeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MemberDeleteCommandParser.java
@@ -3,26 +3,26 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.MemberDeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new MemberDeleteCommand object
  */
-public class DeleteCommandParser implements Parser<DeleteCommand> {
+public class MemberDeleteCommandParser implements Parser<MemberDeleteCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns an DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the MemberDeleteCommand
+     * and returns an MemberDeleteCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public DeleteCommand parse(String args) throws ParseException {
+    public MemberDeleteCommand parse(String args) throws ParseException {
         try {
             Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
+            return new MemberDeleteCommand(index);
         } catch (ParseException pe) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, MemberDeleteCommand.MESSAGE_USAGE), pe);
         }
     }
 
--- src/test/java/seedu/address/logic/parser/MemberDeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/MemberDeleteCommandParserTest.java
@@ -16,7 +16,7 @@ import seedu.address.logic.commands.MemberDeleteCommand;
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */
-public class DeleteCommandParserTest {
+public class MemberDeleteCommandParserTest {
 
     private MemberDeleteCommandParser parser = new MemberDeleteCommandParser();
 
